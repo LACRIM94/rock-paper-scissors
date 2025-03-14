164c9d2a6d4b167007be6eb500013145b15ac6d0
LACRIM94 <anasmestour@gmail.com>
2025-03-14 18:42:49 +0100

    Write the logic for the human choice

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function getComputerChoice(value) {
-    Math.floor(Math.random() * value);
-    if (value === 0) {
-        console.log("rock");
-    } else if (value === 1) {
-        console.log("paper");
+function getComputerChoice() {
+    let x = Math.floor(Math.random() * 3);
+    if (x === 0) {
+        return "rock";
+    } else if (x === 1) {
+        return "paper";
     } else {
-        console.log("scissors")
+        return "scissors";
     } 
 }
 
-console.log(getComputerChoice(3));
+//console.log(getComputerChoice());
+
+function getHumanChoice() {
+    let sign = window.prompt("rock, paper or scissors");
+
+    if (sign === "rock") {
+        return "rock";
+    } else if (sign === "paper") {
+        return "paper";
+    } else if (sign === "scissors") {
+        return "scissor";
+    } else {
+        return "Not a valid input";
+    }
+
+} //console.log(getHumanChoice());
