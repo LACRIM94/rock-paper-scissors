a9b8cfeb68857fdaa40ebd4a3a9d96dbe7e2fe26
LACRIM94 <anasmestour@gmail.com>
2025-03-15 15:47:24 +0100

    Add code for the single round play and fix typo

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function getHumanChoice() {
     } else if (sign === "paper") {
         return "paper";
     } else if (sign === "scissors") {
-        return "scissor";
+        return "scissors";
     } else {
         return "Not a valid input";
     }
@@ -28,3 +28,38 @@ function getHumanChoice() {
 
 let humanScore = 0;
 let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+
+    if (humanChoice.toLowerCase() === "rock" && computerChoice === "rock") {
+        console.log("It's a draw");
+    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "paper") {
+        computerScore++; console.log("You lose! Paper beats Rock");
+    } else if (humanChoice.toLowerCase() === "rock" && computerChoice === "scissors") {
+        humanScore++; console.log("You won! Rock beats Scissors");
+
+    } else if (humanChoice === "paper" && computerChoice === "paper") {
+        console.log("It's a draw");
+    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "scissors") {
+        computerScore++; console.log("You lose! Scissors beats Paper");
+    } else if (humanChoice.toLowerCase() === "paper" && computerChoice === "rock") {
+        humanScore++; console.log("You won! Paper beats rock");
+
+    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "scissors") {
+        console.log("It's a draw");
+    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "rock") {
+        computerScore++; console.log("You lose! Rock beats Scissors");
+    } else if (humanChoice.toLowerCase() === "scissors" && computerChoice === "paper") {
+        humanScore++; console.log("You won! Scissors beats Paper");
+
+    } else {
+        return "Not a valid input";
+    }
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection); 
+console.log(humanScore);
+console.log(computerScore);
